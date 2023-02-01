86d1a46d36b0f7a9cd6e8821bdbdbf6c0ac6e0dc
Samsvill <samantha.sharid@gmail.com>
2023-01-31 23:42:08 -0500

    backend sam

--- scraping.rb
+++ scraping.rb
@@ -2,18 +2,34 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-cervantes = 'https://www.cervantesvirtual.com/buscador/?tipo=palabras
-_todas&busqueda=combinada&ftipo=texto&q=juvenil&orden=
-PONDERACION&limit=50&vista=expandida'
-datosCervantes = URI.open(cervantes)
-cervantesParseado = Nokogiri::HTML(datosCervantes.read)
-lista_items = cervantesParseado.css('.contenido').css('.col-lg-9').css('.item-obra')
-docu = CSV.open('libros.csv','a')
+link = 'https://nostarch.com/catalog.htm'
+datosHTML = URI.open(link)
+datosParseados = Nokogiri::HTML(datosHTML.read)
+bookList = datosParseados.css('.view-content')
 
-lista_items.css('.datos-item-obra').each do |itemObra|
-    strip = itemObra.css('.titulo').inner_text.split("-")
-    prueba = itemObra.css('dd').inner_text
-    titulos = strip[0].lstrip().split("/")
-    docu<<[titulos[0]]
-    
+class Book
+  attr_accessor :title, :subt, :author, :price
+   def initialize(titulo, subt, autor, pr)
+     @title = titulo.to_s
+     @subt = subt.to_s
+     @author = autor.to_s
+     @price = pr.to_s
+   end
+
+  def guardar()
+    CSV.open('ejemplo.csv','a') do |csv|
+      csv<< [@title+"/"+@subt+"/"+@author+"/"+@price]
+    end
+  end
+end
+
+bookList.css(".product-info-wrapper").each do |post|
+  title = post.css(".product-title").inner_text
+  subt = post.css(".product-subtitle").inner_text
+  author = post.css(".product-author").inner_text
+  price = post.css(".uc-price").inner_text
+  
+  book = Book.new(title,subt,author,price)
+  book.guardar
+  
 end
